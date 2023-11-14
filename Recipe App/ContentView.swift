22b4dfb0491c642b12a9e174487288f2934cd79c
Stefon <stefongholston@gmail.com>
2023-11-13 19:19:28 -0600

    Initial Commit

---
+++ Recipe App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Recipe App
+//
+//  Created by Stefon Gholston on 11/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Recipe App/Recipe_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_AppApp.swift
+//  Recipe App
+//
+//  Created by Stefon Gholston on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
